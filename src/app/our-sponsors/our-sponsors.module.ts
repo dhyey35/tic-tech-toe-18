9a9a250ba992cc91a54f4f390d06fd1a39637fe2
jainam <jainams662@gmail.com>
2018-10-07 01:30:54 +0530

    no change

--- src/app/our-sponsors/our-sponsors.module.ts
+++ src/app/our-sponsors/our-sponsors.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { OurSponsorsRoutingModule } from './our-sponsors-routing.module';
 import { OurSponsorsComponent } from './our-sponsors.component';
 import { SharedModule } from '@shared';
+import {ChartModule} from 'primeng/chart';
 
 @NgModule({
     imports: [
         CommonModule,
         OurSponsorsRoutingModule,
         SharedModule,
+        ChartModule
     ],
     declarations: [OurSponsorsComponent]
 })
